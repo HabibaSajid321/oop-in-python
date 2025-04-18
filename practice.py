51e6589d2b52fa35d6fe8806c959faefcc36c5dd
HabibaSajid321 <habibachhudhary434@gmail.com>
2025-04-18 17:16:26 +0500

    Oop concepts notes

---
+++ practice.py
@@ -0,0 +1,44 @@
+a = 7
+print(type(a))
+# in python everything is an object
+# This will result <class 'int'>
+# here a is the object of class 'int'
+
+L = [1,2,3,4]
+L.append(5)
+print(L)
+
+L = 7
+# L.app
+
+# Obbject Literal 
+
+# this is the original syntax of the object 
+# for example class is Car and WangonR is the object The syntax is: 
+# WangonR = Car()
+
+# But as we know list is also an object but we create the list like :
+# This is the object litterals way:
+List = [1,2,4] 
+# : here myList is an object of list class but the syntax is different
+# Reason is we have object literals for Built in data types to create objects in the easiest way
+# We can Also create list like this 
+# 
+# This is the original way 
+myList = list()
+myList.append(1)
+print(myList)
+
+# creating string 
+City = str()
+print(type(City))
+
+
+# Method vs Function
+# -- Method is a special function written inside a class
+# -- Function is a normal function 
+# Len is a function len(L) 
+#  We call the function and method in different ways:
+# ----- Calling method 
+#  L.append(1) : this is a method
+#  
