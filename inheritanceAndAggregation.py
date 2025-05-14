c735557b9c293e988c6888b8eb88f5b94a09c7cd
HabibaSajid321 <90784842+HabibaSajid321@users.noreply.github.com>
2025-05-15 00:19:12 +0500

    Inheritance concepts

--- inheritanceAndAggregation.py
+++ inheritanceAndAggregation.py
@@ -25,14 +25,20 @@ student1.login()
 # As we know child class can also inherit the constructor lets understand with the code.
 
 class Car:
-    def __init__(self, brand, model):
+    def __init__(self, brand, model, price):
         self.brand = brand
         self.model = model
+        self.__price = price
 
 class newCar(Car):
     pass
 
-s = newCar("Civic", "2020")
-print(s.brand, s.model)
+s = newCar("Civic", "2020", "8000")
+print(s.brand, s.model, s.__price)
+s.__price()
+
+
+
+#  Now lets verify is child class can access the private members.
 
         
