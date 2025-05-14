4c4efbc57253570684d4e31361ba7946894944ba
HabibaSajid321 <90784842+HabibaSajid321@users.noreply.github.com>
2025-05-14 23:26:58 +0500

    inheritance

---
+++ inheritanceAndAggregation.py
@@ -0,0 +1,38 @@
+# using inheritance the child can access -Data Members, -Methods, -Construtor
+# But it cannot access the private data.
+# Lets understand with the code 
+
+class User:
+    def login(self):
+        print("login")
+
+    def register(self):
+        print("register")
+
+class Student(User):
+    def enroll(self):
+        print("enroll")
+
+    def review(self):
+        print("review")
+
+student1 = Student()
+student1.enroll()
+student1.login()
+
+# but this is one way. the parent class cannot access the data of the child class.
+
+# As we know child class can also inherit the constructor lets understand with the code.
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+class newCar(Car):
+    pass
+
+s = newCar("Civic", "2020")
+print(s.brand, s.model)
+
+        
